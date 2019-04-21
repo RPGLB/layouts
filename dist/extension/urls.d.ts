c3a4aa2d87202e774c5a1890b711068966636344
Vinay Sarpeshkar <vsarpeshkar@gmail.com>
2019-04-21 01:22:57 -0700

    Remove tracker URLs that won't be used.

--- dist/extension/urls.d.ts
+++ dist/extension/urls.d.ts
@@ -1,14 +1,11 @@
 export interface IGDQUrls {
-    readonly ads: string;
     readonly allBids: string;
     readonly allPrizes: string;
     readonly currentBids: string;
     readonly currentPrizes: string;
-    readonly interviews: string;
     readonly runners: string;
     readonly runs: string;
     readonly total: string;
-    readonly login: string;
 }
 export declare const GDQUrls: IGDQUrls;
 //# sourceMappingURL=urls.d.ts.map
