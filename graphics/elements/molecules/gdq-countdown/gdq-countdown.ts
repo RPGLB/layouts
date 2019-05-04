34f97bb40d608f3c50450c9ec6a063da14622735
Hoishin <hoishinxii@gmail.com>
2019-05-04 14:32:27 -0700

    I mean now it works

--- graphics/elements/molecules/gdq-countdown/gdq-countdown.ts
+++ graphics/elements/molecules/gdq-countdown/gdq-countdown.ts
@@ -1,12 +1,15 @@
 import {NowPlaying} from '../../../../src/types/schemas/nowPlaying';
+import { Countdown } from '../../../../src/types/schemas/countdown';
 
 const {customElement} = Polymer.decorators;
 
 const nowPlaying = nodecg.Replicant<NowPlaying>('nowPlaying');
+const countdownTimer = nodecg.Replicant<Countdown>('nowPlaying');
 
 @customElement('gdq-countdown')
 export default class RPGLBOmnibarBreakinfoElement extends Polymer.Element {
 	music = ''
+	timer = ''
 
 	connectedCallback() {
 		super.connectedCallback();
@@ -15,6 +18,9 @@ export default class RPGLBOmnibarBreakinfoElement extends Polymer.Element {
 			nowPlaying.on('change', newVal => {
 				this.music = `${newVal.game || '?'} - ${newVal.title || '?'}`
 			});
+			countdownTimer.on('change', ({minutes, seconds}) => {
+				this.timer = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`
+			})
 		});
 	}
 }
