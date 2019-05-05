e078b349ecdac040a813edb55d94cb59ef038a48
Vinay Sarpeshkar <vsarpeshkar@gmail.com>
2019-05-05 02:34:53 -0600

    Add pre-show mode to title updater.

--- src/extension/twitch-title-updater.ts
+++ src/extension/twitch-title-updater.ts
@@ -9,14 +9,9 @@ const nodecg = nodecgApiContext.get();
 const log = new nodecg.Logger(`${nodecg.bundleName}:twitch`);
 const currentRun = nodecg.Replicant<GDQTypes.Run>('currentRun');
 let lastLongName: string;
+let lastCategory: string;
 
-currentRun.on('change', newVal => {
-	if (newVal.longName === lastLongName) {
-		return;
-	}
-
-	log.info('Updating Twitch title and game to', newVal.longName);
-	lastLongName = newVal.longName;
+function sendTitle(title: string, game: string) {
 	request({
 		method: 'put',
 		uri: `https://api.twitch.tv/kraken/channels/${nodecg.bundleConfig.twitch.channelId}`,
@@ -29,14 +24,38 @@ currentRun.on('change', newVal => {
 		body: {
 			channel: {
 				// tslint:disable-next-line:no-invalid-template-strings
-				status: nodecg.bundleConfig.twitch.titleTemplate.replace('${gameName}', newVal.longName),
-				game: newVal.longName
+				status: title,
+				game: game
 			}
 		},
 		json: true
 	}).then(() => {
-		log.info('Successfully updated Twitch title and game to', newVal.longName);
+		log.info('Successfully updated Twitch title and game to', game);
 	}).catch(err => {
 		log.error('Failed updating Twitch title and game:\n\t', err);
 	});
+}
+
+currentRun.on('change', newVal => {
+	if (nodecg.bundleConfig.twitch.pre) {
+		sendTitle('RPG Limit Break 2019 starts 10:30 AM MDT, May 5, 2019!', 'Final Fantasy Type-0 HD');
+		return;
+	}
+
+	if (newVal.longName === lastLongName && newVal.category === lastCategory) {
+		return;
+	}
+
+	log.info('Updating Twitch title and game to', newVal.longName, newVal.category);
+	lastLongName = newVal.longName;
+	lastCategory = newVal.category;
+
+	let title = nodecg.bundleConfig.twitch.titleTemplate.replace('${gameName}', newVal.longName);
+	let cat = ''
+	if (newVal.category) {
+		cat = '(' + newVal.category + ')';
+	}
+	title = title.replace('${category}', cat);
+
+	sendTitle(title, newVal.longName);
 });
