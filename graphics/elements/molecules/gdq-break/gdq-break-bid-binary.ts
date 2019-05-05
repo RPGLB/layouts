2bf1e4625aa36711cfab9485cb905cb77fed39bb
Vinay Sarpeshkar <vsarpeshkar@gmail.com>
2019-05-05 03:01:27 -0600

    Fix slight issues with gdq-break-bids.

--- graphics/elements/molecules/gdq-break/gdq-break-bid-binary.ts
+++ graphics/elements/molecules/gdq-break/gdq-break-bid-binary.ts
@@ -1,5 +1,5 @@
 import {ParentBid} from '../../../../src/types/index';
-import {TimelineLite, Power2} from 'gsap'
+import {TimelineLite, TweenLite, Power2, Power4} from 'gsap'
 
 const {customElement, property} = Polymer.decorators;
 
@@ -13,38 +13,60 @@ export default class GDQBreakBidBinaryElement extends Polymer.Element {
 	bid: ParentBid;
 
 	@property({type: String})
-	optionOneText: string
+	optionOneText: string;
 	@property({type: String})
-	optionTwoText: string
+	optionTwoText: string;
 
 	ready() {
-		super.ready()
+		super.ready();
+		TweenLite.set(this, {opacity: 0});
+	}
 
-		const optionOneBar = this.$.optionOneBar as HTMLDivElement
-		const optionTwoBar = this.$.optionTwoBar as HTMLDivElement
+	enter() {
+		const optionOneBar = this.$.optionOneBar as HTMLDivElement;
+		const optionTwoBar = this.$.optionTwoBar as HTMLDivElement;
 
-		const total = this.bid.rawTotal
-		const [optionOne, optionTwo] = this.bid.options
+		const total = this.bid.rawTotal;
+		const [optionOne, optionTwo] = this.bid.options;
 
-		this.optionOneText = `${optionOne.name} - ${optionOne.total}`
-		this.optionTwoText = `${optionTwo.name} - ${optionTwo.total}`
+		this.optionOneText = `${optionOne.name} - ${optionOne.total}`;
+		this.optionTwoText = `${optionTwo.name} - ${optionTwo.total}`;
 
 		const optionOneBarPercent = optionOne.rawTotal / total * 100
 		const optionTwoBarPercent = 100 - optionOneBarPercent;
 
-		const tl = new TimelineLite()
-		tl.addLabel('start', 0.3)
+		const tl = new TimelineLite();
+
+		tl.to(this, 0.465, {
+			opacity: 1,
+			ease: Power4.easeIn
+		});
+
+		tl.addLabel('start', 0.3);
 		tl.to(
 			optionOneBar,
 			0.5,
 			{width: `${optionOneBarPercent}%`, ease: Power2.easeOut},
 			'start'
-		)
+		);
 		tl.to(
 			optionTwoBar,
 			0.5,
 			{width: `${optionTwoBarPercent}%`, ease: Power2.easeOut},
 			'start'
-		)
+		);
+
+		return tl;
+	}
+
+	exit() {
+		const tl = new TimelineLite();
+
+		tl.to(this, 0.2, {
+			opacity: 0,
+			ease: Power4.easeIn
+		});
+
+		return tl;
 	}
 }
