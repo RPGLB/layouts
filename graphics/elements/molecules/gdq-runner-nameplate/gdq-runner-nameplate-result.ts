a2c6a62039750cad635361ebe639ce4c153ae391
Hoishin <hoishinxii@gmail.com>
2019-05-05 18:51:51 -0600

    individual place & time for races

--- graphics/elements/molecules/gdq-runner-nameplate/gdq-runner-nameplate-result.ts
+++ graphics/elements/molecules/gdq-runner-nameplate/gdq-runner-nameplate-result.ts
@@ -1,11 +1,7 @@
-import {TimelineLite, TweenLite, Power3, Sine} from 'gsap';
+import { TimelineLite, Power3 } from 'gsap';
 
 const {customElement, property} = Polymer.decorators;
 
-/**
- * @customElement
- * @polymer
- */
 @customElement('gdq-runner-nameplate-result')
 export default class GDQNameplateResultElement extends Polymer.Element {
 	@property({type: Boolean, observer: '_showingChanged'})
@@ -29,61 +25,29 @@ export default class GDQNameplateResultElement extends Polymer.Element {
 	@property({type: Boolean, reflectToAttribute: true})
 	forfeit: boolean;
 
-	private readonly _tl = new TimelineLite({autoRemoveChildren: true});
-
-	ready() {
-		super.ready();
-		TweenLite.set(this, {x: 0});
-		TweenLite.set(this.$.cover, {scaleX: 1});
-		TweenLite.set(this.$.place, {scaleX: 0});
-	}
-
-	show() {
-		const anim = new TimelineLite();
-		anim.to(this, 0.5, {
-			x: this.side === 'left' ? '-100%' : '100%',
-			ease: Power3.easeIn
-		});
-
-		anim.to(this.$.cover, 0.5, {
-			scaleX: 0,
-			ease: Power3.easeOut
-		});
-
-		anim.to(this.$.place, 0.182, {
-			scaleX: 1,
-			ease: Sine.easeOut
-		});
-
-		return anim;
-	}
-
-	hide() {
-		const anim = new TimelineLite();
-		anim.to(this.$.place, 0.182, {
-			scaleX: 0,
-			ease: Sine.easeIn
-		});
-
-		anim.to(this.$.cover, 0.5, {
-			scaleX: 1,
-			ease: Power3.easeIn
-		});
-
-		anim.to(this, 0.5, {
-			x: '0%',
-			ease: Power3.easeOut
-		});
-
-		return anim;
-	}
-
-	_showingChanged(newVal: boolean) {
-		const anim = newVal ? this.show() : this.hide();
-		this._tl.add(anim);
-	}
-
-	_calcPlaceText(place: number, forfeit: boolean) {
-		return forfeit ? 'X' : place;
+	_showingChanged(showing: boolean) {
+		if (showing) {
+			const tl = new TimelineLite()
+			tl.to(this, 0.5, {
+				transform: this.side === 'left' ? 'translateX(-100%)' : 'translateX(100%)',
+				ease: Power3.easeInOut
+			})
+			if (!this.forfeit) {
+				tl.to(this.$.placeWrapper, 0.5, {
+					transform: this.side === 'left' ? 'translateX(-20px)' : 'translateX(20px)',
+					ease: Power3.easeInOut
+				})
+			}
+		} else {
+			const tl = new TimelineLite()
+			tl.to(this, 0.5, {
+				transform: 'translateX(0)',
+				ease: Power3.easeInOut
+			})
+			tl.to(this.$.placeWrapper, 0.5, {
+				transform: 'translateX(0)',
+				ease: Power3.easeInOut
+			})
+		}
 	}
 }
